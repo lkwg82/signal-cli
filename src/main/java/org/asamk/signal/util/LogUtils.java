d510dc9d1452b4ef2a7705abd1edfef806147341
Lars K.W. Gohlke <lkwg82@gmx.de>
2018-11-21 15:06:09 +0100

    extracted SendCommand

--- src/main/java/org/asamk/signal/util/LogUtils.java
+++ src/main/java/org/asamk/signal/util/LogUtils.java
@@ -1,13 +1,34 @@
 package org.asamk.signal.util;
 
+import java.util.Arrays;
+
 public class LogUtils {
 
-    public static final boolean DEFAULT = false;
+    static final boolean DEFAULT = false;
     public static boolean DEBUG_ENABLED = DEFAULT;
 
-    public static void debug(String entry) {
+    public static void debug(String... entries) {
         if (DEBUG_ENABLED) {
-            System.err.println("DEBUG " + entry);
+            if (entries == null) {
+                _debug("null args - plz fix");
+            } else {
+                switch (entries.length) {
+                    case 0:
+                        _debug("empty args - not nice");
+                        break;
+                    case 1:
+                        _debug(entries[0]);
+                        break;
+                    default:
+                        String[] args = Arrays.copyOfRange(entries, 1, entries.length);
+                        String format = entries[0];
+                        _debug(String.format(format, args));
+                }
+            }
         }
+    }
+
+    private static void _debug(final String text) {
+        System.err.println("DEBUG " + text);
     }
 }
